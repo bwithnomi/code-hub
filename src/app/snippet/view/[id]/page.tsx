3529127bbbece87ddf61209bde78c0988a9cb612
Abid Noman <abidnoman@Abids-MacBook-Air.local>
2025-06-22 14:39:39 +0500

    dark mode

--- src/app/snippet/view/[id]/page.tsx
+++ src/app/snippet/view/[id]/page.tsx
@@ -6,9 +6,9 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const snippet = await viewSnippetByShareId(id);
   return (
-    <div className="flex items-start justify-center bg-slate-200 h-screen p-8 overflow-scroll">
+    <div className="flex items-start justify-center bg-slate-200 dark:bg-slate-800 h-screen p-8 overflow-scroll">
       {snippet ? (
-        <div className="w-3xl py-8 shadow-2xl px-4 rounded-3xl bg-amber-100">
+        <div className="w-3xl py-8 shadow-2xl px-8 rounded-3xl bg-rose-100 dark:bg-rose-300">
           <p className="text-center font-bold text-xl">{snippet?.title}</p>
           <div className="">
             {snippet?.files.map((f) => (
@@ -26,7 +26,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
           </div>
         </div>
       ) : (
-        <div className="w-3xl py-12 shadow-2xl px-8 rounded-3xl bg-amber-100 flex flex-col items-center justify-center gap-10">
+        <div className="w-3xl py-12 shadow-2xl px-8 rounded-3xl bg-slate-400 dark:bg-rose-300 flex flex-col items-center justify-center gap-10">
           <p className="text-center font-bold text-xl">Oops! The snippet you're looking for either doesn't exist or hasn't been shared publicly.</p>
           <Image src="/private_snippet.svg" width={400} height={400} alt=""></Image>
         </div>
